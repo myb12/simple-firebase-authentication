031b1f9b6cb147a61471e618cc2d0b7cef7eef04
Mohammad <mohammadyasinbappy@outlook.com>
2021-10-06 09:49:34 +0600

    initial commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA3OsdKzGQyy24Y9oDBK0DR270ZD_av8JI",
+    authDomain: "simple-firebase-authenti-d88f0.firebaseapp.com",
+    projectId: "simple-firebase-authenti-d88f0",
+    storageBucket: "simple-firebase-authenti-d88f0.appspot.com",
+    messagingSenderId: "870506350614",
+    appId: "1:870506350614:web:1cae6c61ecdd46ec0ecd37"
+};
+
+export default firebaseConfig;
